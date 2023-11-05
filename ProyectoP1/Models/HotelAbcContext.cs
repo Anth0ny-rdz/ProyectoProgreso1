ce47969d2f310463fb7799d2252f7a98c483866d
ANTHONY <anthony.rodriguez.camuendo@udla.edu.ec>
2023-11-04 20:46:02 -0500

    Adición de la tabla usuarios

---
+++ ProyectoP1/Models/HotelAbcContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoP1.Models;
+
+public partial class HotelAbcContext : DbContext
+{
+    public HotelAbcContext()
+    {
+    }
+
+    public HotelAbcContext(DbContextOptions<HotelAbcContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local); DataBase=HotelABC; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF973A7484CC");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
